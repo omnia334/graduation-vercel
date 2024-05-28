e7dff93a2eaf2abbdd4fcbce37cb7ca614cfe044
omnia334 <110954551+omnia334@users.noreply.github.com>
2024-05-29 02:56:29 +0300

    Delete src directory

--- src/models/patient.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const patientSchema = new mongoose.Schema({
-    patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    assistans: []
-}, { timestamps: true })
-module.exports = mongoose.model('patientModel', patientSchema)
--- src/routers/patient.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const auth = require('../middelware/auth')
-const { addAssistant, deleteAssistant } = require('../controllers/patient')
-route.patch('/addAssistant/:id', auth.user, addAssistant)
-route.delete('/deleteAssistant/:id', auth.user, deleteAssistant)
-module.exports = route
